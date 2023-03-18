182061d9fca8c711ca4df22a554b30349c34fb8b
Anthony Asiimwe <tonyasiimwe14@gmail.com>
2023-03-18 16:04:35 -0700

    Mandatory tasks complete

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
