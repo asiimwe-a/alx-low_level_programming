80d1de70dbb09a85c43654cee114cbf2ba8782a2
Anthony Asiimwe <tonyasiimwe14@gmail.com>
2023-04-06 16:43:46 -0700

    Mandatory tasks

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *factorial - factorial of signed number
+ *@n: pointer to fill block of memory.
+ *Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+
+		return (1);
+
+	else if (n < 0)
+		return (-1);
+	else
+		return (n * factorial(n - 1));
+}
