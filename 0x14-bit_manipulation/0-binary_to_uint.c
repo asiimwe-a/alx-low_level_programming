b6509d9eb82e1841bc7240a39a665af1caf89129
asiimwe-a <tonyasiimwe14@gmail.com>
2023-05-04 14:55:05 -0700

    #include main.h

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,17 +13,14 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-
 	for (x = 0; b[x] != '\0'; x++)
 		if (b[x]  != '0' && b[x] != '1')
 			return (0);
-			
 	for (x = 0; b[x] != '\0'; x++)
 	{
 	val *= 2;
 	if (b[x] == '1')
 	val += 1;
-	
 	}
 	return (val);
 }
